726fce2a89ed48de5958d290dc770f49be7246f8
deanjones <deanjones@gmail.com>
2014-09-16 14:57:21 -0400

    #5: bump to 2.0.9

--- src/com/ait/lienzo/ks/client/HeaderPanel.java
+++ src/com/ait/lienzo/ks/client/HeaderPanel.java
@@ -38,7 +38,7 @@ public class HeaderPanel extends KSContainer
 
         inside.setHeight(60);
 
-        KSSimple title = new KSSimple("Ahomé Lienzo Kitchen Sink v 2.0.8 SNAPSHOT", 1);
+        KSSimple title = new KSSimple("Ahomé Lienzo Kitchen Sink v 2.0.9 SNAPSHOT", 1);
 
         title.setId("HeaderPanel-Title");
 
