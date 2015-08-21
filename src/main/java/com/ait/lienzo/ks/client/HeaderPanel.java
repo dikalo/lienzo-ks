0e24a9a306709142b9f70fa1bd56d4da055dc007
deanjones <deanjones@gmail.com>
2015-08-20 22:06:03 -0400

    2.0.126-RC1

--- src/main/java/com/ait/lienzo/ks/client/HeaderPanel.java
+++ src/main/java/com/ait/lienzo/ks/client/HeaderPanel.java
@@ -38,7 +38,7 @@ public class HeaderPanel extends KSContainer
 
         inside.setHeight(60);
 
-        KSSimple title = new KSSimple("Ahomé Lienzo Kitchen Sink v 2.0.125-RC1", 1);
+        KSSimple title = new KSSimple("Ahomé Lienzo Kitchen Sink v 2.0.126-RC1", 1);
 
         title.setId("HeaderPanel-Title");
 
