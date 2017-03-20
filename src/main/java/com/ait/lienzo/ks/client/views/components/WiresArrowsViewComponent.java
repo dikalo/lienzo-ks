8488b426f7e5fbcdb9fe69cdea6bf126ac32662e
Kirill Gaevskii <kgaevski@redhat.com>
2017-03-20 13:06:41 +0100

    Clean up formatting and redundancies

--- src/main/java/com/ait/lienzo/ks/client/views/components/WiresArrowsViewComponent.java
+++ src/main/java/com/ait/lienzo/ks/client/views/components/WiresArrowsViewComponent.java
@@ -16,12 +16,26 @@
 
 package com.ait.lienzo.ks.client.views.components;
 
-import com.ait.lienzo.client.core.shape.*;
-import com.ait.lienzo.client.core.shape.wires.*;
+import static com.ait.lienzo.client.core.shape.wires.LayoutContainer.Layout.CENTER;
+
+import com.ait.lienzo.client.core.shape.Circle;
+import com.ait.lienzo.client.core.shape.IContainer;
+import com.ait.lienzo.client.core.shape.Layer;
+import com.ait.lienzo.client.core.shape.MultiPath;
+import com.ait.lienzo.client.core.shape.MultiPathDecorator;
+import com.ait.lienzo.client.core.shape.OrthogonalPolyLine;
+import com.ait.lienzo.client.core.shape.Star;
+import com.ait.lienzo.client.core.shape.wires.IConnectionAcceptor;
+import com.ait.lienzo.client.core.shape.wires.IContainmentAcceptor;
+import com.ait.lienzo.client.core.shape.wires.MagnetManager;
+import com.ait.lienzo.client.core.shape.wires.WiresConnection;
+import com.ait.lienzo.client.core.shape.wires.WiresConnector;
+import com.ait.lienzo.client.core.shape.wires.WiresContainer;
+import com.ait.lienzo.client.core.shape.wires.WiresMagnet;
+import com.ait.lienzo.client.core.shape.wires.WiresManager;
+import com.ait.lienzo.client.core.shape.wires.WiresShape;
 import com.ait.lienzo.client.core.types.Point2DArray;
 import com.ait.lienzo.ks.client.views.AbstractViewComponent;
-
-import static com.ait.lienzo.client.core.shape.wires.LayoutContainer.Layout.CENTER;
 
 public class WiresArrowsViewComponent extends AbstractViewComponent
 {
@@ -29,13 +43,13 @@ public class WiresArrowsViewComponent extends AbstractViewComponent
     {
         Layer layer = new Layer();
 
-        WiresManager wires_manager = WiresManager.get(layer);
+        WiresManager wiresManager = WiresManager.get(layer);
 
         double w = 100;
 
         double h = 100;
 
-        wires_manager.setConnectionAcceptor(new IConnectionAcceptor()
+        wiresManager.setConnectionAcceptor(new IConnectionAcceptor()
         {
             @Override
             public boolean headConnectionAllowed(WiresConnection head, WiresShape shape)
@@ -117,7 +131,7 @@ public class WiresArrowsViewComponent extends AbstractViewComponent
 
         // A shape can only contain shapes of different letters for UserData
 
-        wires_manager.setContainmentAcceptor(new IContainmentAcceptor()
+        wiresManager.setContainmentAcceptor(new IContainmentAcceptor()
         {
             @Override
             public boolean containmentAllowed(WiresContainer parent, WiresShape child)
@@ -139,28 +153,28 @@ public class WiresArrowsViewComponent extends AbstractViewComponent
         WiresShape wiresShape0 = new WiresShape(new MultiPath().rect(0, 0, w, h).setStrokeColor("#CC0000")).setX(400).setY(400).setDraggable(true);
         wiresShape0.getContainer().setUserData("A");
         wiresShape0.addChild(new Circle(30), CENTER);
-        wires_manager.register( wiresShape0 );
-        wires_manager.getMagnetManager().createMagnets( wiresShape0 );
+        wiresManager.register(wiresShape0);
+        wiresManager.getMagnetManager().createMagnets(wiresShape0);
 
         WiresShape wiresShape1 = new WiresShape(new MultiPath().rect(0, 0, w, h).setStrokeColor("#00CC00")).setX(50).setY(50).setDraggable(true);
         wiresShape1.getContainer().setUserData("A");
         wiresShape1.addChild(new Star(5, 15, 40), CENTER);
-        wires_manager.register( wiresShape1 );
-        wires_manager.getMagnetManager().createMagnets( wiresShape1 );
+        wiresManager.register(wiresShape1);
+        wiresManager.getMagnetManager().createMagnets(wiresShape1);
 
         WiresShape wiresShape2 = new WiresShape(new MultiPath().rect(0, 0, 300, 200).setStrokeColor("#0000CC")).setX(50).setY(100).setDraggable(true);
         wiresShape2.getContainer().setUserData("B");
-        wires_manager.register( wiresShape2 );
-        wires_manager.getMagnetManager().createMagnets( wiresShape2 );
+        wiresManager.register(wiresShape2);
+        wiresManager.getMagnetManager().createMagnets(wiresShape2);
 
         // bolt
         String svg = "M 0 100 L 65 115 L 65 105 L 120 125 L 120 115 L 200 180 L 140 160 L 140 170 L 85 150 L 85 160 L 0 140 Z";
         WiresShape wiresShape3 = new WiresShape(new MultiPath(svg).setStrokeColor("#0000CC")).setX(50).setY(300).setDraggable(true);
         wiresShape3.getContainer().setUserData("B");
-        wires_manager.register( wiresShape3 );
-        wires_manager.getMagnetManager().createMagnets( wiresShape3 );
+        wiresManager.register(wiresShape3);
+        wiresManager.getMagnetManager().createMagnets(wiresShape3);
 
-        connect(layer, wiresShape1.getMagnets(), 3, wiresShape0.getMagnets(), 7, wires_manager);
+        connect(wiresShape1.getMagnets(), wiresShape0.getMagnets(), wiresManager);
 
         getLienzoPanel().add(layer);
 
@@ -169,10 +183,10 @@ public class WiresArrowsViewComponent extends AbstractViewComponent
         getWorkingContainer().add(getLienzoPanel());
     }
 
-    private void connect(Layer layer, MagnetManager.Magnets magnets0, int i0_1, MagnetManager.Magnets magnets1, int i1_1, WiresManager wiresManager)
+    private void connect(MagnetManager.Magnets magnets0, MagnetManager.Magnets magnets1, WiresManager wiresManager)
     {
-        WiresMagnet m0_1 = (WiresMagnet) magnets0.getMagnet(i0_1);
-        WiresMagnet m1_1 = (WiresMagnet) magnets1.getMagnet(i1_1);
+        WiresMagnet m0_1 = magnets0.getMagnet(3);
+        WiresMagnet m1_1 = magnets1.getMagnet(7);
 
         double x0, x1, y0, y1;
 
@@ -193,19 +207,19 @@ public class WiresArrowsViewComponent extends AbstractViewComponent
         y0 = m0_1.getControl().getY();
         x1 = m1_1.getControl().getX();
         y1 = m1_1.getControl().getY();
-        line = createLine(layer, 0, 0, x0, y0, (x0 + ((x1 - x0) / 2)), (y0 + ((y1 - y0) / 2)), x1, y1);
+        line = createLine(x0, y0, (x0 + ((x1 - x0) / 2)), (y0 + ((y1 - y0) / 2)), x1, y1);
         line.setHeadOffset(head.getBoundingBox().getHeight());
         line.setTailOffset(tail.getBoundingBox().getHeight());
 
-        WiresConnector connector0 = new WiresConnector( m0_1, m1_1, line, new MultiPathDecorator(head), new MultiPathDecorator(tail) );
-        wiresManager.register( connector0 );
+        WiresConnector connector0 = new WiresConnector(m0_1, m1_1, line, new MultiPathDecorator(head), new MultiPathDecorator(tail));
+        wiresManager.register(connector0);
 
         head.setStrokeWidth(5).setStrokeColor("#0000CC");
         tail.setStrokeWidth(5).setStrokeColor("#0000CC");
         line.setStrokeWidth(5).setStrokeColor("#0000CC");
     }
 
-    private final OrthogonalPolyLine createLine(Layer layer, double x, double y, final double... points)
+    private OrthogonalPolyLine createLine(final double... points)
     {
         return new OrthogonalPolyLine(Point2DArray.fromArrayOfDouble(points)).setCornerRadius(5).setDraggable(true);
     }
